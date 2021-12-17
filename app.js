2a14f439d6e72e1996a414693556d24533a59c08
LoaiMasri <s12010098@stu.najah.edu>
2021-12-17 18:34:38 +0200

    Add Project Base
    26/11/2021

---
+++ app.js
@@ -0,0 +1,4 @@
+// Here JS Code
+const IPIFY_API='at_TBQYyxtV1kufTPzRnLUvrSkOTz3Px'
+const MAP_API='pk.eyJ1IjoibG9haW1hc3JpIiwiYSI6ImNrd2dudWtiZzBxNmYzMXBtOGYxYm1jY24ifQ.s90TP5x2nJhNbPUW25ixNw'
+//https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}
