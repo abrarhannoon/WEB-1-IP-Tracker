e4976f3cbaec9acc4eb03e71ae92b7e45a6dc02e
abrarhannoon <92648528+abrarhannoon@users.noreply.github.com>
2021-12-20 15:35:08 +0200

    javascript complement
    14-12-2021

--- app.js
+++ app.js
@@ -3,38 +3,42 @@ const IPIFY_API='at_5Kmiy5agVMtCidp33lQj4hjVLDsyX'
 const MAP_API='pk.eyJ1IjoibG9haW1hc3JpIiwiYSI6ImNrd2dudWtiZzBxNmYzMXBtOGYxYm1jY24ifQ.s90TP5x2nJhNbPUW25ixNw'
 //https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}
 //test Gitconst IPIFY_API='at_NCrQUdaY64KrgHegDBl87doCY2hsS'
-const MyIP = localStorage.getItem('MyIP');
-const latt = localStorage.getItem('lat');
-const lngg = localStorage.getItem('lng');
+let IP1=document.querySelector('#IP')
+let Locations=document.querySelector('#Location')
+let TimeZone=document.querySelector('#TimeZone')
+let ISP=document.querySelector('#ISP')
 //window.addEventListener('load',GetYourIp)
-window.addEventListener('load',IntialResult)
+window.addEventListener('load',GetYourIp)
+document.getElementById('btn').addEventListener('click', getIP);
 
 function GetYourIp(){
     fetch('https://api.ipify.org?format=json')
     .then(response => response.json())
     .then(data =>{
-        localStorage.setItem('MyIP', data.ip)
+        IntialResult(data.ip)
 })
 }
-function IntialResult(){
-    let IP=document.querySelector('#IP')
-    let Locations=document.querySelector('#Location')
-    let TimeZone=document.querySelector('#TimeZone')
-    let ISP=document.querySelector('#ISP')
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}`)
+function IntialResult(IP){
+
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}&ipAddress=${IP}`)
     .then(response => response.json()).then(data =>{
-    localStorage.setItem('MyIP', data.ip),
-    localStorage.setItem('lat', data.location.lat),
-    localStorage.setItem('lng', data.location.lng),
-    IP.textContent=data.ip,
+    IP1.textContent=data.ip,
     Locations.textContent=data.location.city,
     TimeZone.textContent=data.location.timezone,
-    ISP.textContent=data.isp
+    ISP.textContent=data.isp;
+    let latt = data.location.lat;
+    let lngg = data.location.lng;
+        mapp(latt, lngg)
     }).catch(error => {
         console.log('ThereIs A Error');
     })
 }
-let map = L.map('map').setView([latt, lngg], 13)
+function mapp(latt,lngg){
+    var container = L.DomUtil.get('map');
+    if (container != null) {
+        container._leaflet_id = null;
+    }
+    let map = L.map('map').setView([latt, lngg], 13)
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
@@ -46,3 +50,10 @@ var greenIcon = L.icon({
     iconUrl: './Images/icon-location.svg'
 });
 var marker =L.marker([latt, lngg], {icon: greenIcon}).addTo(map);
+}
+
+function getIP() {
+    let input = document.getElementById('input').value;
+    IntialResult(input);
+
+}
