f2ff57959fe3cbb56d7f0266fac5f600563010d7
LoaiMasri <s12010098@stu.najah.edu>
2021-12-22 18:03:03 +0200

    async function
    conver promises into async function
    and add hit enter function

--- app.js
+++ app.js
@@ -1,59 +1,78 @@
 // Here JS Code
+
 const IPIFY_API='at_5Kmiy5agVMtCidp33lQj4hjVLDsyX'
 const MAP_API='pk.eyJ1IjoibG9haW1hc3JpIiwiYSI6ImNrd2dudWtiZzBxNmYzMXBtOGYxYm1jY24ifQ.s90TP5x2nJhNbPUW25ixNw'
-//https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}
-//test Gitconst IPIFY_API='at_NCrQUdaY64KrgHegDBl87doCY2hsS'
-let IP1=document.querySelector('#IP')
-let Locations=document.querySelector('#Location')
-let TimeZone=document.querySelector('#TimeZone')
-let ISP=document.querySelector('#ISP')
-//window.addEventListener('load',GetYourIp)
-window.addEventListener('load',GetYourIp)
-document.getElementById('btn').addEventListener('click', getIP);
+const IP1=qs('#IP')
+const Locations=qs('#Location')
+const TimeZone=qs('#TimeZone')
+const ISP=qs('#ISP')
+const SearchBtn=qs('#locationbutton')
+const TextBox = qs('#input');
 
-function GetYourIp(){
-    fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data =>{
-        IntialResult(data.ip)
-})
-}
-function IntialResult(IP){
+window.addEventListener('load',GetMyIP)
+SearchBtn.addEventListener('click', getIP)
+TextBox.addEventListener('keypress',HitEnter)
 
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}&ipAddress=${IP}`)
-    .then(response => response.json()).then(data =>{
-    IP1.textContent=data.ip,
-    Locations.textContent=data.location.city,
-    TimeZone.textContent=data.location.timezone,
-    ISP.textContent=data.isp;
-    let latt = data.location.lat;
-    let lngg = data.location.lng;
-        mapp(latt, lngg)
-    }).catch(error => {
-        console.log('ThereIs A Error');
-    })
+async function GetMyIP(){
+    let response=await fetch('https://api.ipify.org?format=json')
+    let data= await response.json()
+    Result(data.ip)
 }
-function mapp(latt,lngg){
-    var container = L.DomUtil.get('map');
-    if (container != null) {
-        container._leaflet_id = null;
+
+async function Result(IP){
+    try {
+        let response =await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${IPIFY_API}&ipAddress=${IP}`)
+        let data=await response.json()
+        SetValue(data)
+    } catch (err) {
+        alert('Please Input A Valid IP')
+        console.error(err)
     }
-    let map = L.map('map').setView([latt, lngg], 13)
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+}
+
+function mapp(latt,lngg){
+    let container = L.DomUtil.get('map');
+    if (container != null) {container._leaflet_id = null;}
+    let map = L.map('map',{ zoomControl: false }).setView([latt, lngg], 13)
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: MAP_API
  }).addTo(map);
-var greenIcon = L.icon({
-    iconUrl: './Images/icon-location.svg'
+let myIcon = L.icon({
+    iconUrl: './Images/icon-location.svg',
+    iconSize: [40, 50],
+    iconAnchor: [17, 50]
 });
-var marker =L.marker([latt, lngg], {icon: greenIcon}).addTo(map);
+let marker =L.marker([latt, lngg], {icon: myIcon}).addTo(map);
+}
+
+function SetValue(data){
+    IP1.textContent=data.ip
+    Locations.textContent=`${data.location.city},${data.location.country} ${data.location.postalCode}`
+    TimeZone.textContent=data.location.timezone
+    ISP.textContent=data.isp
+    let latt = data.location.lat
+    let lngg = data.location.lng
+    mapp(latt, lngg)
+}
+
+function HitEnter(event){
+    if(event.keyCode===13){
+        event.preventDefault()
+        getIP();
+    }
 }
 
 function getIP() {
-    let input = document.getElementById('input').value;
-    IntialResult(input);
+    event.preventDefault()
+    let text=TextBox.value;
+    Result(text);
+}
 
+
+function qs(element) {
+    return document.querySelector(element)
 }
